f447fddcc2be4c330843cd420c13b807cd3f07b2
Leondro Lio <leondrolio@gmail.com>
2018-03-29 21:35:47 +0100

    This is a proof engine. It does proof.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Travis CI Proof for Discord
+This is a Travis "proof".
+This code executes a known code with a secret that only Travis CI holds
+
+This proof is invalid if:
+- The commit cannot be found on GitHub
+- The code is fabricated to not reflect the true statistics
+- The Eris client is a different one to what is on NPM
+
+Please compare this code to the one found at:
+https://github.com/moustacheminer/travis-proof-discord
+
+For an example, please visit the latest commit for the Travis CI build information.
---
+++ index.js
@@ -0,0 +1,37 @@
+const Eris = require('eris');
+
+const client = new Eris(process.env.TEST_TOKEN, {
+    maxShards: 'auto'
+});
+
+console.log(`
+This is a Travis "proof".
+This document only proves that a Client ID has the following statistics.
+Please view the commit on GitHub to validate the code.
+
+Waiting for the client to connect...
+`);
+
+if (!process.env.TEST_TOKEN) {
+    console.error(`
+No token was provided to the Eris client!
+Use the environment variable "TEST_TOKEN" to pass a token to the script.
+    `);
+    process.exit(1);
+}
+
+client.on('ready', () => {
+    console.log(`
+== START OF "PROOF" ==
+
+This "proof" certifies that a bot with ID "${client.user.id}" holds the following statistics
+
+Guild Count:          ${client.guilds.size}
+Users Visible to Bot: ${client.users.size}
+
+== END OF "PROOF" ==
+    `);
+    process.exit(0);
+});
+
+client.connect();
